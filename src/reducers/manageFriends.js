841352185626c55c574add398674ecc4a715be2e
AidiRi <rivera.aidi@gmail.com>
2019-08-02 14:21:22 -0700

    Done.

--- src/reducers/manageFriends.js
+++ src/reducers/manageFriends.js
@@ -1,2 +1,25 @@
 export function manageFriends(state, action){
+  switch (action.type) {
+    case "ADD_FRIEND":
+      return (
+        {...state,
+        friends: [
+          ...state.friends,
+          action.friend]}
+      )
+
+    case "REMOVE_FRIEND":
+    debugger;
+      const trueFriends = state.friends.filter(friend => friend.id
+        !== action.id)
+        // THIS DON'T MAKE SENSE!!!
+      return (
+        {...state,
+        friends: trueFriends
+        }
+      )
+
+    default:
+      return state
+  }
 }
